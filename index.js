df34c2a2890683169b26251b951f0d2ae6716ada
Sadiquehmd <91723984+Sadiquehmd@users.noreply.github.com>
2024-09-04 14:48:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var cors = require('cors')
 connectMongo();
 const app = express()
 const port = process.env.PORT || 4000
-app.use(cors())
+app.use(cors({
+  origin: 'https://main--inotesby.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
